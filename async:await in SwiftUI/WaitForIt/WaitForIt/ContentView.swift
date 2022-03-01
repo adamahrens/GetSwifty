b60f7ca3fa488647591f054698f50e62d6ab9e21
Adam Ahrens <adam.aahrens@gmail.com>
2022-02-28 20:52:49 -0600

    Working with async/await and actors

--- async:await in SwiftUI/WaitForIt/WaitForIt/ContentView.swift
+++ async:await in SwiftUI/WaitForIt/WaitForIt/ContentView.swift
@@ -12,31 +12,86 @@ struct ContentView: View {
   @StateObject var service = JokeService()
   
   var body: some View {
-    ZStack {
-      Text(service.joke)
-        .multilineTextAlignment(.center)
-        .padding(.horizontal)
+    ScrollView {
+      Section("Current") {
+        Text(service.joke)
+          .fontWeight(.bold)
+          .multilineTextAlignment(.center)
+          .padding(.horizontal)
+          .frame(maxHeight: 200)
+      }
       
-      VStack {
-        Spacer()
-        
-        Button { service.fetchJoke() } label: {
-          Text("Fetch Joke")
-            .padding(.bottom)
-            .opacity(service.isFetching ? 0 : 1)
-            .overlay {
-              if service.isFetching {
-                ProgressView()
-              }
-            }
+      Button {
+        Task.init(priority: .userInitiated) {
+          try? await service.fetchJoke()
         }
+      } label: {
+        Text("Fetch Joke")
+          .padding(.bottom)
+          .opacity(service.isFetching ? 0 : 1)
+          .overlay {
+            if service.isFetching {
+              ProgressView()
+            }
+          }
+      }
+      
+      if service.previousJokes.count > 1 {
+        Section("Previous") {
+          ForEach(service.previousJokes, id: \.self) { joke in
+            Text(joke)
+              .fontWeight(.light)
+              .multilineTextAlignment(.center)
+              .padding(.horizontal)
+              .frame(maxHeight: 200)
+          }
+        }
+      }
+    }
+    
+    /*
+    Group {
+     
+     
+      Button {
+
+        // Feels like calling
+        // DispatchQueue.global().async
+        
+//          Deprecated
+//          async {
+//            try? await service.fetchJoke()
+//          }
+        
+        Task.init(priority: .userInitiated) {
+          try? await service.fetchJoke()
+        }
+      } label: {
+        Text("Fetch Joke")
+          .padding(.bottom)
+          .opacity(service.isFetching ? 0 : 1)
+          .overlay {
+            if service.isFetching {
+              ProgressView()
+            }
+          }
+      }
+    }
+     */
+      
+//      List(service.previousJokes, id: \.self) { joke  in
+//        Text(joke)
+//      }
+      
+        
+      
+        /*
         AsyncImage(url: URL(string: "https://files.betamax.raywenderlich.com/attachments/collections/194/e12e2e16-8e69-432c-9956-b0e40eb76660.png")) { image in
           image.resizable()
         } placeholder: {
           Color.red
         }.frame(width: 128, height: 128)
-      }
-    }
+         */
   }
 }
 
--- async:await in SwiftUI/WaitForIt/WaitForIt/JokeService.swift
+++ async:await in SwiftUI/WaitForIt/WaitForIt/JokeService.swift
@@ -32,9 +32,8 @@
 
 import Foundation
 
-class JokeService: ObservableObject {
-  @Published private(set) var joke = "Joke appears here"
-  @Published private(set) var isFetching = false
+actor JokeServiceActor {
+  private var loadedJoke = Joke.empty
   
   private var url: URL {
     // swiftlint:disable:next force_unwrapping
@@ -49,14 +48,107 @@ class JokeService: ObservableObject {
     components.setQueryItems(with: ["category": "dev"])
     return components
   }
+  
+  func load() async throws -> Joke {
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard
+      let httpResponse = response as? HTTPURLResponse,
+      httpResponse.statusCode.isSuccessfulHtttpStatusCode
+    else { throw DownloadError.statusNotOk }
+    
+    guard
+      let joke = try? JSONDecoder().decode(Joke.self, from: data)
+    else { throw DownloadError.decoderError }
+    
+    loadedJoke = joke
+    return loadedJoke
+  }
+}
+
+class JokeService: ObservableObject {
+  @Published private(set) var joke = "Joke appears here"
+  @Published private(set) var isFetching = false
+  @Published private(set) var previousJokes = [String]()
+  
+  private let store = JokeServiceActor()
 
   public init() { }
 }
 
-extension JokeService {
-  func fetchJoke() {
-    isFetching = true
+enum DownloadError: Error {
+  case statusNotOk
+  case decoderError
+}
 
+extension Int {
+  var isSuccessfulHtttpStatusCode: Bool {
+    200..<300 ~= self
+  }
+}
+
+extension JokeService {
+  
+  // With this we call load and this thread is suspended
+  // Freeing up UI
+  // load is called on background thread and when it's done
+  // Picks up back on the main thread
+  @MainActor
+  func fetchJoke() async throws {
+    isFetching = true
+    
+    // Called when succesful or an error thrown
+    defer { isFetching = false }
+    
+    // Problem now is we're using a background thread
+    // Even though we started by calling from a SwiftUI Main thread
+    let loadedJoke = try await store.load()
+    joke = loadedJoke.value
+    previousJokes.append(loadedJoke.value)
+    
+    /*
+     * Th_s called concurrent binding because parent task continues
+     * execution after creating a child task with the data(from: url) call on another
+     * thread. _d inherits parent's priority level and local variables.
+     *
+    async let (data, response) = URLSession.shared.data(from: url)
+    try! await (data, response)
+     */
+    
+    /*
+     * Sequential binding is combing the async and await to one line
+     * This won't create a child task like above. Instead it runs sequentially in the parent
+     */
+    
+    /*
+     * Moving to Actor
+     
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard
+      let httpResponse = response as? HTTPURLResponse,
+      httpResponse.statusCode.isSuccessfulHtttpStatusCode
+    else { throw DownloadError.statusNotOk }
+    
+    guard
+      let joke = try? JSONDecoder().decode(Joke.self, from: data)
+    else { throw DownloadError.decoderError }
+     
+    *
+    */
+
+    // But what about dispatch to Main
+    // Not to Fear
+    // The Button was created with MainActor which is just like DispatchQueue.main
+    // All SwiftUI Views run on MainActor
+    // So when we made the async call with this it delivers on the main thread
+    // if you want something to run not on MainActor
+    // You need to create your own!
+//    self.joke = joke.value
+    
+    /*
+     * Old way before async/await changes
+     *
     URLSession.shared.dataTask(with: url) { data, response, error in
       defer {
         DispatchQueue.main.async {
@@ -75,11 +167,14 @@ extension JokeService {
       print("Joke fetch failed: \(error?.localizedDescription ?? "Unknown error")")
     }
     .resume()
+     */
   }
 }
 
 struct Joke: Codable {
   let value: String
+  
+  static let empty = Joke(value: "")
 }
 
 public extension URLComponents {
